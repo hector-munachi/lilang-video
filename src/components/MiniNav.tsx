eb7724601661d03db3ff6cf2289ca69876eb293d
hector-munachi <victornothector@gmail.com>
2023-10-25 15:04:17 +0100

    quick fix

--- src/components/MiniNav.tsx
+++ src/components/MiniNav.tsx
@@ -20,7 +20,11 @@ export default function MiniNav () {
       sections={[
         {
           items: [
-            <EuiHeaderLogo></EuiHeaderLogo>,
+            <EuiHeaderLogo>
+               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
+              </svg>
+            </EuiHeaderLogo>,
             <EuiHeaderLinks aria-label="App navigation dark theme example">
               <EuiHeaderLink isActive
                 onClick={() => navigate("/flashcards")}
@@ -28,19 +32,19 @@ export default function MiniNav () {
               <EuiHeaderLink
                 onClick={() => navigate("/quizmain")}
               >Quiz</EuiHeaderLink>
-              <EuiHeaderLink iconType="help"> Help</EuiHeaderLink>
+              <EuiHeaderLink>Coming soon</EuiHeaderLink>
             </EuiHeaderLinks>,
           ],
         },
         {
           items: [
-            <EuiBadge
-              color={euiTheme.colors.darkestShade}
-              iconType="arrowDown"
-              iconSide="right"
-            >
-              Production logs
-            </EuiBadge>,
+            // <EuiBadge
+            //   color={euiTheme.colors.darkestShade}
+            //   iconType="arrowDown"
+            //   iconSide="right"
+            // >
+            //   Production logs
+            // </EuiBadge>,
           ],
         },
       ]}
