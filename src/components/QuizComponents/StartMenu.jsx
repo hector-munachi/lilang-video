095d9e7326deddf6059fac3a20685f6f06db958d
hector-munachi <victornothector@gmail.com>
2023-10-25 15:09:09 +0100

    quick fix

--- src/components/QuizComponents/StartMenu.jsx
+++ src/components/QuizComponents/StartMenu.jsx
@@ -79,9 +79,6 @@ const StartMenu = ({ initialize }) => {
       <div className="p-4 shadow-lg w-96">
       <div className='flex justify-center items-center mb-5'>
         <h1 className='text-4xl mr-2 text-purple-400'>Word Quiz</h1>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
-      </svg>
       </div>
         <div className="mb-4">
           <EuiComboBox
